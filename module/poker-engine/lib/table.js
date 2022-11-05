6bfc0af66550cfe43f86a37ed54a8350e8516870
ghostcommander0102 <ghostcommander0102@outlook.com>
2022-11-05 15:59:31 +1000

    Update

--- module/poker-engine/lib/table.js
+++ module/poker-engine/lib/table.js
@@ -5,7 +5,7 @@ var Game = require('./game.js').Game;
 var utils = require('./utils.js');
 var EventEmitter = require('events').EventEmitter;
 var colors = require('colors');
-var Ranker = require('/module/handranker');
+var Ranker = require('../../handranker');
 colors.setTheme({
     info: 'bgGreen',
     help: 'cyan',
