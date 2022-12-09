30857c9c75e115cf64b85c68185edd2d232363a9
ghostcommander0102 <ghostcommander0102@outlook.com>
2022-12-10 04:31:19 +1000

    update

--- room_manager/loginmanager.js
+++ room_manager/loginmanager.js
@@ -28,11 +28,11 @@ exports.setsocketio = function (socketio) {
 	io = socketio;
 };
 exports.CheckVersionCode = function (socket) {
-	let emitdata = { result: "7.3" };
+	let emitdata = { result: "7.4" };
 	socket.emit("CHECK_VERSION_CODE_RESULT", emitdata);
 };
 exports.LogIn = function (socket, userInfo) {
-	if (userInfo.version == null || userInfo.version != "7.3") {
+	if (userInfo.version == null || userInfo.version != "7.4") {
 		let emitdata = { result: "failed" };
 		socket.emit("GET_LOGIN_RESULT", emitdata);
 	} else {
@@ -132,7 +132,7 @@ function makeRandom(min, max) {
 }
 exports.SignUp = function (socket, data) {
 	let lManager = this;
-	if (data.version == null || data.version != "7.3") {
+	if (data.version == null || data.version != "7.4") {
 		socket.emit("GET_REGISTER_RESULT", { result: "failed" });
 	} else {
 		let collection = database.collection("User_Data");
