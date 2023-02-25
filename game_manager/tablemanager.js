ca187d0093e51aa4b168b0770559f6dc3d3de31c
HoneyBear1120 <87842727+ghostcommander0102@users.noreply.github.com>
2023-02-26 09:43:00 +1000

    update bot

--- game_manager/tablemanager.js
+++ game_manager/tablemanager.js
@@ -343,7 +343,7 @@ TableManager.prototype.actionBot = function (player) {
                 goodcards = true;
             }
             let hardLuck = Math.random() * 100;
-            if(hardLuck >= 90) {
+            if (hardLuck >= 75) {
                 goodcards = true;
             }
             //this.hardCount--;
@@ -423,97 +423,79 @@ TableManager.prototype.actionBot = function (player) {
                     }
                 } else {
                     if (canCheck) info.action = "check";
-                    else if (canCall && ((this.isRaise && (botgoodcards || goodcards)) || !this.isRaise)) {
-                        if (!this.isRaise && canCall) {
+                    let num1 = Math.floor(Math.random() * 10) + 1;
+                    if (num1 > 4) {
+                        if (canCall) {
                             info.action = "call";
                             info.bet = call;
-                        } else if (this.isRaise) {
-                            if (botgoodcards) {
-                                info.action = "call";
-                                info.bet = call;
-                            }
-                        }
-                    }
-                    if (goodcards == true && this.hardCount == 0) {
-                        if (canCheck) info.action = "check";
-                        if (this.bigBlinds.indexOf(this.bigBlind) == -1) {
-                            if (goodcards == true || player.win == true) {
-                                let num1 = Math.floor(Math.random() * 10) + 1;
-                                if (num1 > 4) {
-                                    if (canCall) {
-                                        info.action = "call";
-                                        info.bet = call;
-                                    } else gamelog.showlog(">>> ERROR1".err + " roomID:" + this.id);
+                        } else gamelog.showlog(">>> ERROR1".err + " roomID:" + this.id);
+                    } else {
+                        let randomNumber =
+                            raiseRandom[
+                            Math.floor(
+                                Math.random() * raiseRandom.length
+                            )
+                            ];
+                        if (canRaise) {
+                            info.legal_bet = this.legalBet;
+                            info.bet = randomNumber * minRaise;
+                            let maxBet = max_Bet;
+                            let currentBet = current_Bet;
+                            if (info.bet < player.chips) {
+                                if (maxBet - currentBet == info.bet) {
+                                    info.action = "call";
                                 } else {
-                                    let randomNumber =
-                                        raiseRandom[
-                                        Math.floor(
-                                            Math.random() * raiseRandom.length
-                                        )
-                                        ];
-                                    if (canRaise) {
-                                        info.legal_bet = this.legalBet;
-                                        info.bet = randomNumber * minRaise;
-                                        let maxBet = max_Bet;
-                                        let currentBet = current_Bet;
-                                        if (info.bet < player.chips) {
-                                            if (maxBet - currentBet == info.bet) {
-                                                info.action = "call";
-                                            } else {
-                                                info.action = "raise";
-                                                this.isRaise = true;
-                                            }
-                                            info.legal_bet = info.bet - call;
-                                        } else {
-                                            let buff = 0;
-                                            let index = 0;
-                                            for (
-                                                let i = 0;
-                                                i < this.table.players.length;
-                                                i++
-                                            ) {
-                                                gamelog.showlog(this.table.players[i] + " roomID:" + this.id);
-                                                if (
-                                                    this.table.players[i] !=
-                                                    undefined &&
-                                                    this.table.players[i].chips !=
-                                                    undefined &&
-                                                    buff <=
-                                                    this.table.players[i].chips
-                                                ) {
-                                                    buff =
-                                                        this.table.players[i].chips;
-                                                    index = i;
-                                                }
-                                            }
-                                            if (buff < player.chips) {
-                                                if (
-                                                    this.table.game.bets.length > 0
-                                                ) {
-                                                    info.bet =
-                                                        buff +
-                                                        this.table.game.bets[
-                                                        index
-                                                        ] -
-                                                        player.GetBet();
-                                                } else {
-                                                    info.bet =
-                                                        buff - player.GetBet();
-                                                }
-                                            } else {
-                                                info.bet = player.chips;
-                                            }
-
-                                            info.action = "allin";
-                                        }
-                                    } else {
-                                        if (canCall) {
-                                            info.action = "call";
-                                            info.bet = call;
-                                        } else gamelog.showlog(">>> ERROR2".err + " roomID:" + this.id);
+                                    info.action = "raise";
+                                    this.isRaise = true;
+                                }
+                                info.legal_bet = info.bet - call;
+                            } else {
+                                let buff = 0;
+                                let index = 0;
+                                for (
+                                    let i = 0;
+                                    i < this.table.players.length;
+                                    i++
+                                ) {
+                                    gamelog.showlog(this.table.players[i] + " roomID:" + this.id);
+                                    if (
+                                        this.table.players[i] !=
+                                        undefined &&
+                                        this.table.players[i].chips !=
+                                        undefined &&
+                                        buff <=
+                                        this.table.players[i].chips
+                                    ) {
+                                        buff =
+                                            this.table.players[i].chips;
+                                        index = i;
                                     }
                                 }
+                                if (buff < player.chips) {
+                                    if (
+                                        this.table.game.bets.length > 0
+                                    ) {
+                                        info.bet =
+                                            buff +
+                                            this.table.game.bets[
+                                            index
+                                            ] -
+                                            player.GetBet();
+                                    } else {
+                                        info.bet =
+                                            buff - player.GetBet();
+                                    }
+                                } else {
+                                    info.bet = player.chips;
+                                }
+
+                                info.action = "allin";
                             }
+                        } else {
+                            if (canCall && botgoodcards) {
+                                info.action = "call";
+                                info.bet = call;
+                            } else gamelog.showlog(">>> ERROR2".err + " roomID:" + this.id);
                         }
                     }
                 }
